7a0fc76b7656ac25b154ef4b282cb2c44c3dcb41
Ahstell <torut1638@gmail.com>
2025-06-26 16:49:27 -0500

    Declaracion de variables y tipos de comentarios en java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,12 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Word...!");
+
+        int numero = 100;
+        //Comentarios
+        /**
+         * Bloque de comentarios
+         * Bloque de comentarios
+         * **/
     }
 }
