0c541064897b685e49d318bee14796a85ec3ef18
Ahstell <torut1638@gmail.com>
2025-06-27 12:28:57 -0500

    Se creo una clase donde se aplica la regla 50 30 20 al salario ingresado por el usuario

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,13 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Word...!");
-
-        int numero = 100;
-        //Comentarios
-        /**
-         * Bloque de comentarios
-         * Bloque de comentarios
-         * **/
+        System.out.println("Hello World");
     }
 }
---
+++ src/main/java/org/example/Regla_50_30_20.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Regla_50_30_20 {
+    public static void main(String[] args) {
+        // 50% Nececesidades
+        // 30% Cosas que queremos
+        // 20% Ahorros
+
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("Ingrese el salario que gana mensualmente: ");
+        float salario = teclado.nextFloat();
+        float necesidades = salario * 0.5f;
+        float otrasCosas = salario * 0.3f;
+        float ahorros = salario * 0.2f;
+        System.out.println("Deberias de asignar tu salario en las siguientes");
+        System.out.println("Necesidades: $ " + necesidades);
+        System.out.println("Otras cosas: $ " + otrasCosas);
+        System.out.println("Ahorros: $ " + ahorros);
+    }
+}
