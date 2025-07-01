5e38caedbc329441994e9fa3f9403c2f3d989809
Ahstell <torut1638@gmail.com>
2025-06-30 21:00:26 -0500

    Usamos if y else para saber si una persona es mayor de edad o no

---
+++ src/main/java/org/example/MayorDeEdad.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class MayorDeEdad {
+    public static void main(String[] args) {
+        int edad = 25;
+        if(edad >= 18) {
+            System.out.println("El edad es mayor que 18");
+        }else {
+            System.out.println("El es menor que 18 años");
+        }
+    }
+}
