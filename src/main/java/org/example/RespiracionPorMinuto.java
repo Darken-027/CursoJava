2039bb782b403589efe12aca1ef95420a671f867
Ahstell <torut1638@gmail.com>
2025-07-01 07:05:50 -0500

    Se creo el codigo para saber que tipo de nivel de actividad tiene un usuario

--- src/main/java/org/example/RespiracionPorMinuto.java
+++ src/main/java/org/example/RespiracionPorMinuto.java
@@ -1,7 +1,22 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class RespiracionPorMinuto {
     public static void main(String[] args) {
-        
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Ingrese su edad");
+        int edad = teclado.nextInt();
+
+        System.out.println("Ingrese su nivel de actividad");
+        System.out.println("1) Sedentario");
+        System.out.println("2) Moderado");
+        System.out.println("3) Activo");
+        int nivelActividad = teclado.nextInt();
+
+        System.out.println("Ingrese su sexo");
+        System.out.println("1) Hombre");
+        System.out.println("2) Mujer");
+        int sexo = teclado.nextInt();
     }
 }
