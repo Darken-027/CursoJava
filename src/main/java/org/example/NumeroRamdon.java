a446a491f173f4ca2398413dc9ac3d2904d37f39
Ahstell <torut1638@gmail.com>
2025-07-01 14:53:37 -0500

    Se mofico la clase NumeroRandom Para dar otro ejemplo en codigo para tener un numero random en un rango predeterminado

--- src/main/java/org/example/NumeroRamdon.java
+++ src/main/java/org/example/NumeroRamdon.java
@@ -1,10 +1,14 @@
 package org.example;
 
+import java.util.Random;
+
 public class NumeroRamdon {
     public static void main(String[] args) {
         // Calculando un numero Random
         int numero = 0;
-        numero = (int) (Math.random() * 100);
+        // numero = (int) (Math.random() * 100);
+        Random random = new Random();
+        numero = random.nextInt(100);
         System.out.println("Numero: " + numero);
     }
 }
