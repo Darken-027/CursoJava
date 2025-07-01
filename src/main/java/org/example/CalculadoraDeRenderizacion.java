082e844f1cd6b0f07d63b171e30afed557fc9004
Ahstell <torut1638@gmail.com>
2025-06-30 20:05:06 -0500

    Se crea la clase CalculadoraDeRenderizacion y se declara el problema

---
+++ src/main/java/org/example/CalculadoraDeRenderizacion.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class CalculadoraDeRenderizacion {
+    public static void main(String[] args) {
+        /*
+         *  Calculadora de renderizado
+         *  Para renderizar una pelicula 3D se
+         *  contrata una empresa externa de render.
+         *  Tienes que armar una calculadora que
+         *  permita calcular el costo del render.
+         *  Las variables son:
+         *  - Precio por minuto de render
+         *  - Cantidad de minutos a renderizar
+         * */
+    }
+}
