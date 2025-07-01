a0e2113cd08fa653ae8f1970b813472bfdc2024a
Ahstell <torut1638@gmail.com>
2025-06-30 19:37:29 -0500

    Creacion de la clase calcular IVA y declaracion del problema

---
+++ src/main/java/org/example/CalcularIVA.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class CalcularIVA {
+    public static void main(String[] args) {
+        /*
+        *   Calcular IVA
+        *   Lee un numero por teclado que pida
+        *   el precio de un producto (Puede tener decimales)
+        *   y calcule el precio final con IVA sera una
+        *   constante que sera deñ 21%
+        * */
+
+        
+    }
+}
