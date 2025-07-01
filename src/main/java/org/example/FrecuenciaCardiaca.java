452cfc08d74b512c09fb545765c479f42e40671f
Ahstell <torut1638@gmail.com>
2025-07-01 07:23:43 -0500

    Se implemento el codigo para saber cual es tu tasa de respiraciones por minuto

--- src/main/java/org/example/FrecuenciaCardiaca.java
+++ src/main/java/org/example/FrecuenciaCardiaca.java
@@ -1,7 +1,18 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class FrecuenciaCardiaca {
     public static void main(String[] args) {
-        
+        System.out.println("Ingrese la cantidad de respiraciones por minuto (RPM)");
+        Scanner teclado = new Scanner(System.in);
+        int respiraciones = teclado.nextInt();
+
+        if(respiraciones >= 18 && respiraciones <= 20){
+            System.out.println("La tasa de respiraciones por minutos esta en el rango normal");
+        }
+        else if(respiraciones < 18 && respiraciones > 20){
+            System.out.println("La tasa de respiraciones por minuto esta en el rango bajo");
+        }
     }
 }
