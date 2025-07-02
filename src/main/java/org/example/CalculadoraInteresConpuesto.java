e9b66c5ac4642269574654c19810d2461ee9904f
Ahstell <torut1638@gmail.com>
2025-07-02 14:57:25 -0500

    Se genero el codigo para hacer calcular el interes compuesto

--- src/main/java/org/example/CalculadoraInteresConpuesto.java
+++ src/main/java/org/example/CalculadoraInteresConpuesto.java
@@ -1,4 +1,20 @@
 package org.example;
 
 public class CalculadoraInteresConpuesto {
+    public static void main(String[] args) {
+        float capitalInicial = 1000;
+        float adicionAnual = 10000;
+        float tasaInteres = 0.05f; //5%
+        int periodoAnos = 5;
+
+        float montoFinal = capitalInicial;
+        for (int i = 0; i < periodoAnos; i++){
+            float crecimientoAnual = montoFinal * tasaInteres;
+            montoFinal += crecimientoAnual;
+            montoFinal += adicionAnual;
+
+        }
+        System.out.println(montoFinal);
+    }
+
 }
