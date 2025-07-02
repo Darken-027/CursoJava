1d0c502e3d4dd0b99e51fe01ba7b2a2a3a889339
Ahstell <torut1638@gmail.com>
2025-07-02 14:30:13 -0500

    La clase NumerosMultiplos fue modificada

--- src/main/java/org/example/NumeroMultiplos.java
+++ src/main/java/org/example/NumeroMultiplos.java
@@ -5,7 +5,7 @@ public class NumeroMultiplos {
         for (int i= 1; i < 100;){
             boolean esMultiploDe4 = i % 4 == 0;
             boolean esMultiploDe7 = i % 7 == 0;
-            if (esMultiploDe4 && esMultiploDe7){
+            if (esMultiploDe4 || esMultiploDe7){
                 System.out.println(i);
             }
         }
