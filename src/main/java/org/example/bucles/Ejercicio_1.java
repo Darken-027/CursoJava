9dca55fa70d46c6ec1a8e7840b59cf0917bdec34
Ahstell <torut1638@gmail.com>
2025-07-02 16:58:36 -0500

    Ejercicio_1 resolviendo el problema

---
+++ src/main/java/org/example/bucles/Ejercicio_1.java
@@ -0,0 +1,28 @@
+package org.example.bucles;
+
+import java.util.Scanner;
+
+public class Ejercicio_1 {
+    public static void main(String[] args) {
+        /*
+        Escribe un programa que permita generar la tabla de multiplicar de
+        un número entero positivo N,comenzando desde 1.
+        Si el usuario escribe un número incorrecto, el programa
+        no se ejecuta. En cambio, pregunta de nuevo por la información hasta
+        que el dato ingresado sea correcto
+         */
+
+        Scanner t = new Scanner(System.in);
+
+        int n;
+        System.out.print("Ingrese un numero entero positivo: ");
+        n = t.nextInt();
+
+        if(n > 0){
+            System.out.println("Exelente");
+        }
+        else{
+            System.out.println("El numero ingresado no es correcto");
+        }
+    }
+}
