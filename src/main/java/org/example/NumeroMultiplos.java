4446d7584b2da5ee247244bf62029eaeeba7e281
Ahstell <torut1638@gmail.com>
2025-07-02 14:28:38 -0500

    Se genero el codigo para saber si un numero es multiplo de 4 y 7

--- src/main/java/org/example/NumeroMultiplos.java
+++ src/main/java/org/example/NumeroMultiplos.java
@@ -2,6 +2,12 @@ package org.example;
 
 public class NumeroMultiplos {
     public static void main(String[] args) {
-        
+        for (int i= 1; i < 100;){
+            boolean esMultiploDe4 = i % 4 == 0;
+            boolean esMultiploDe7 = i % 7 == 0;
+            if (esMultiploDe4 && esMultiploDe7){
+                System.out.println(i);
+            }
+        }
     }
 }
