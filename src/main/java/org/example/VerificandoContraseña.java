7dbf28b96a7676339a0cc6699ede3954a0678a42
Ahstell <torut1638@gmail.com>
2025-07-02 12:58:28 -0500

    Resolucion de la clase VerificandoContraseña

--- src/main/java/org/example/VerificandoContraseña.java
+++ src/main/java/org/example/VerificandoContraseña.java
@@ -1,7 +1,28 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class VerificandoContraseña {
     public static void main(String[] args) {
+        int intentos = 3;
+        while(intentos < 3) {
+            String password  = "chocolate";
+            System.out.println("Ingrese la contraseña");
+            Scanner teclado = new Scanner(System.in);
+            String userPassword = teclado.nextLine();
 
+            if(password.equals(userPassword)) {
+                System.out.println("Inicio seccion corectamente");
+                break;
+            } else {
+                System.out.println("Password incorrecto");
+                intentos--;
+                if(intentos == 0){
+                    System.out.println("Cuenta bloqueada por 60 minutos");
+                }else{
+                    System.out.println("Intete de nuevo por favor");
+                }
+            }
+        }
     }
 }
