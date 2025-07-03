7ccb2247ea917506084bf149c56f4fe4041a1f9e
Ahstell <torut1638@gmail.com>
2025-07-03 14:41:43 -0500

    Mostrando los numeros multiplos de 20 de forma desendente usando el bucle while

---
+++ src/main/java/org/example/bucles/Ejercicio_5.java
@@ -0,0 +1,15 @@
+package org.example.bucles;
+
+public class Ejercicio_5 {
+    public static void main(String[] args) {
+
+        int i = 320;
+
+        while(i > 160){
+            i--;
+            if(i % 20 == 0){
+                System.out.println(i);
+            }
+        }
+    }
+}
