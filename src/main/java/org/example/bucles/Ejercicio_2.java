364366966ddaed63d7bc9dbb97fe4ea8f3c0e961
Ahstell <torut1638@gmail.com>
2025-07-03 13:05:26 -0500

    Implementacion de bucle for

--- src/main/java/org/example/bucles/Ejercicio_2.java
+++ src/main/java/org/example/bucles/Ejercicio_2.java
@@ -1,7 +1,24 @@
 package org.example.bucles;
 
+import java.util.Scanner;
+
 public class Ejercicio_2 {
     public static void main(String[] args) {
-        
+        Scanner lector = new Scanner(System.in);
+
+        int n;
+        float resultado = 0;
+
+        System.out.print("Ingrese un numero entero positivo: ");
+        n = lector.nextInt();
+
+        if(n > 0){
+            for(int i; i <= n; i++){
+                
+            }
+        }
+        else{
+            System.out.println("El numero " + n + " no " + "es correcto");
+        }
     }
 }
