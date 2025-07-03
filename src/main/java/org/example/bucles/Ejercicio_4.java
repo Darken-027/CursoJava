befd333eed90d63a672c222eeb207288b5cbd341
Ahstell <torut1638@gmail.com>
2025-07-03 14:35:11 -0500

    Implementacion del bucle

--- src/main/java/org/example/bucles/Ejercicio_4.java
+++ src/main/java/org/example/bucles/Ejercicio_4.java
@@ -2,6 +2,9 @@ package org.example.bucles;
 
 public class Ejercicio_4 {
     public static void main(String[] args) {
-        
+
+        for (int i = 320; i >= 160; i--){
+            System.out.println(i);
+        }
     }
 }
