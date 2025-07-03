b9da3127fb28b117eb38bb3200c6e0bd61e90247
Ahstell <torut1638@gmail.com>
2025-07-03 14:36:42 -0500

    Aplicando la condicion al bucle para que imprima los numeros de 20 en 20

--- src/main/java/org/example/bucles/Ejercicio_4.java
+++ src/main/java/org/example/bucles/Ejercicio_4.java
@@ -4,7 +4,9 @@ public class Ejercicio_4 {
     public static void main(String[] args) {
 
         for (int i = 320; i >= 160; i--){
-            System.out.println(i);
+           if(i % 20 == 0){
+               System.out.println(i);
+           }
         }
     }
 }
