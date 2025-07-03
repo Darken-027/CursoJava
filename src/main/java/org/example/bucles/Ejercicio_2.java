073c005eab34d75b9909eabb7b2c8854ff454b82
Ahstell <torut1638@gmail.com>
2025-07-03 14:17:43 -0500

    Imprimiendo los numeros multiplos de 5 utilizando for

--- src/main/java/org/example/bucles/Ejercicio_2.java
+++ src/main/java/org/example/bucles/Ejercicio_2.java
@@ -1,24 +1,11 @@
 package org.example.bucles;
 
-import java.util.Scanner;
-
 public class Ejercicio_2 {
     public static void main(String[] args) {
-        Scanner lector = new Scanner(System.in);
-
-        int n;
-        float resultado = 0;
-
-        System.out.print("Ingrese un numero entero positivo: ");
-        n = lector.nextInt();
-
-        if(n > 0){
-            for(int i; i <= n; i++){
-                
-            }
-        }
-        else{
-            System.out.println("El numero " + n + " no " + "es correcto");
-        }
+      for (int i = 1; i <= 100; i++){
+          if (i % 5 == 0){
+              System.out.println(i);
+          }
+      }
     }
 }
