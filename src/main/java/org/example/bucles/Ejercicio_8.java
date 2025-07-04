35f2ed8b0e8a08532e4aed1cc6293dcea2334b36
Ahstell <torut1638@gmail.com>
2025-07-04 14:09:34 -0500

    Clse modificada

--- src/main/java/org/example/bucles/Ejercicio_8.java
+++ src/main/java/org/example/bucles/Ejercicio_8.java
@@ -1,8 +1,18 @@
 package org.example.bucles;
 
+import java.util.Scanner;
+
 public class Ejercicio_8 {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Introduce un numero");
+        int numero = sc.nextInt();
+
+        while (numero >= 0){
+            System.out.println("Introduce un numero");
+            numero = sc.nextInt();
+        }
     }
 }
  
