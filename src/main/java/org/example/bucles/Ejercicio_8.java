14f7c6ada94dc9eaf6b08460307cf51fe73d3290
Ahstell <torut1638@gmail.com>
2025-07-04 14:22:43 -0500

    Clase modificada

--- src/main/java/org/example/bucles/Ejercicio_8.java
+++ src/main/java/org/example/bucles/Ejercicio_8.java
@@ -5,12 +5,16 @@ import java.util.Scanner;
 public class Ejercicio_8 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        double suma = 0;
+        int contador = 0;
 
-        System.out.println("Introduce un numero");
+        System.out.print("Introduce un numero: ");
         int numero = sc.nextInt();
 
         while (numero >= 0){
-            System.out.println("Introduce un numero");
+            contador++;
+            System.out.println("La media es = " + suma/contador);
+            System.out.print("Introduce un numero: ");
             numero = sc.nextInt();
         }
     }
