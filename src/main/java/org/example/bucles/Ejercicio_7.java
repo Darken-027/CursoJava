f8ec8f1aedf78f57f5182f4fe7670be3dc250bc7
Ahstell <torut1638@gmail.com>
2025-07-04 13:06:29 -0500

    Modificando la clase

--- src/main/java/org/example/bucles/Ejercicio_7.java
+++ src/main/java/org/example/bucles/Ejercicio_7.java
@@ -6,6 +6,10 @@ public class Ejercicio_7 {
     public static void main(String[] args) {
         Scanner  sc = new Scanner(System.in);
         System.out.print("Introduce un numero: ");
+        int numero = sc.nextInt();
         
+        for (int i = 0; i < 10; i++){
+            System.out.println("Multiplica : " + i + " X " + numero);
+        }
     }
 }
