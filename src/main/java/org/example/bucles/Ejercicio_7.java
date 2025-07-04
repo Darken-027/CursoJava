52a1437368062e2116f1f4d4a8d0d4441e088c1a
Ahstell <torut1638@gmail.com>
2025-07-04 13:02:51 -0500

    Se creo la clase Ejercicio_7

---
+++ src/main/java/org/example/bucles/Ejercicio_7.java
@@ -0,0 +1,11 @@
+package org.example.bucles;
+
+import java.util.Scanner;
+
+public class Ejercicio_7 {
+    public static void main(String[] args) {
+        Scanner  sc = new Scanner(System.in);
+        System.out.print("Introduce un numero: ");
+        
+    }
+}
