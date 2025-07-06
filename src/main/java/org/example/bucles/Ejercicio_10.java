56ddb7b2d5178b8881dd4193d32312bfe4f7db92
Ahstell <torut1638@gmail.com>
2025-07-05 23:46:57 -0500

    Se creo el bucle for

--- src/main/java/org/example/bucles/Ejercicio_10.java
+++ src/main/java/org/example/bucles/Ejercicio_10.java
@@ -5,5 +5,8 @@ import java.util.Scanner;
 public class Ejercicio_10 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        for(int i = 0; i < 10; i++){
+            
+        }
     }
 }
