877fe01138ff395c29d298e9f068e6bad93ced27
Ahstell <torut1638@gmail.com>
2025-07-05 23:46:00 -0500

    Se creo la clase Scanner

--- src/main/java/org/example/bucles/Ejercicio_10.java
+++ src/main/java/org/example/bucles/Ejercicio_10.java
@@ -1,7 +1,9 @@
 package org.example.bucles;
 
+import java.util.Scanner;
+
 public class Ejercicio_10 {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
     }
 }
