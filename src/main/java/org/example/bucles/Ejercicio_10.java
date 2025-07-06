08b567a774e065f9834c647fc403a45a98abf2d4
Ahstell <torut1638@gmail.com>
2025-07-05 23:48:05 -0500

    Se creo la linea introduce un numero

--- src/main/java/org/example/bucles/Ejercicio_10.java
+++ src/main/java/org/example/bucles/Ejercicio_10.java
@@ -6,6 +6,7 @@ public class Ejercicio_10 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         for(int i = 0; i < 10; i++){
+            System.out.println("Introduce un numero: ");
             
         }
     }
