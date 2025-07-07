623febaa6886df2e0386626c11b788d4352d9cb0
Ahstell <torut1638@gmail.com>
2025-07-06 23:36:02 -0500

    Inicializacion de un arreglo

--- src/main/java/org/example/areglos/EjemploArreglo.java
+++ src/main/java/org/example/areglos/EjemploArreglo.java
@@ -3,7 +3,9 @@ package org.example.areglos;
 public class EjemploArreglo {
     public static void main(String[] args) {
 
+        //Declaracion de un arreglo
         String[] nombres;
-
+        //Inicializacion de un arreglo
+        nombres = new  String[4];
     }
 }
