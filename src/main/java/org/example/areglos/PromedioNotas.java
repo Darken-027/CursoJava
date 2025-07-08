d61f9dcdf30dc9e11f008af7b01f30a0e6a5943d
Ahstell <torut1638@gmail.com>
2025-07-07 23:40:03 -0500

    Invocamos la clase Scanner y creamos la variable cantidadAlumnos

--- src/main/java/org/example/areglos/PromedioNotas.java
+++ src/main/java/org/example/areglos/PromedioNotas.java
@@ -1,13 +1,20 @@
 package org.example.areglos;
 
+import java.util.Scanner;
+
 public class PromedioNotas {
     public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        
         System.out.print("Ingrese la cantidad de Alumnos: ");
-
+        int cantidadAlumnos = teclado.nextInt();
+        
         System.out.print("Ingrese el nombre del Alumno: ");
         System.out.print("Ingrese la nota del primer semestre: ");
         System.out.print("Ingrese la nota del segundo semeste: ");
         System.out.println("El promedio es: X");
+
+
 
 
         
