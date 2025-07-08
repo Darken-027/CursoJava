a0c514f52cdd441d4fcadbabd3e40aa2fb6b0e63
Ahstell <torut1638@gmail.com>
2025-07-07 23:32:49 -0500

    Ingrese el nombre del Alumno pedimos al usuario

--- src/main/java/org/example/areglos/PromedioNotas.java
+++ src/main/java/org/example/areglos/PromedioNotas.java
@@ -3,5 +3,7 @@ package org.example.areglos;
 public class PromedioNotas {
     public static void main(String[] args) {
         System.out.print("Ingrese la cantidad de Alumnos: ");
+
+        System.out.print("Ingrese el nombre del Alumno: ");
     }
 }
