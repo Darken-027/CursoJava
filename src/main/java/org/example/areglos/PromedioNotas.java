e3db760429e92e60704bdc5a8dd22cd6d5070f6d
Ahstell <torut1638@gmail.com>
2025-07-07 23:31:16 -0500

    Se pide al usuario ingrese la cantidad de alumnos

--- src/main/java/org/example/areglos/PromedioNotas.java
+++ src/main/java/org/example/areglos/PromedioNotas.java
@@ -2,6 +2,6 @@ package org.example.areglos;
 
 public class PromedioNotas {
     public static void main(String[] args) {
-        
+        System.out.print("Ingrese la cantidad de Alumnos: ");
     }
 }
