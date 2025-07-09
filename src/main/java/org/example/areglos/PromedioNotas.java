9cd887a9c438a674ea406346c1040eb83c59f798
Ahstell <torut1638@gmail.com>
2025-07-08 23:20:59 -0500

    Colocamos todo el codigo que se iterara dentro del bucle for

--- src/main/java/org/example/areglos/PromedioNotas.java
+++ src/main/java/org/example/areglos/PromedioNotas.java
@@ -12,26 +12,18 @@ public class PromedioNotas {
         String[] alumnos = new String[cantidadAlumnos];
 
         for(int i = 0; i < cantidadAlumnos; i++){
-            
+            System.out.print("Ingrese el nombre del Alumno: ");
+            String nombreAlumno = teclado.next();
+
+            System.out.print("Ingrese la nota del primer semestre: ");
+            float nota1 = teclado.nextFloat();
+
+            System.out.print("Ingrese la nota del segundo semeste: ");
+            float nota2 = teclado.nextFloat();
+
+            float promedio = (nota1 + nota2) / 2;
+            System.out.println("El promedio de : " + nombreAlumno + " es de: " + promedio);
         }
-        
-        System.out.print("Ingrese el nombre del Alumno: ");
-        String nombreAlumno = teclado.next();
-        
-        System.out.print("Ingrese la nota del primer semestre: ");
-        float nota1 = teclado.nextFloat();
-
-        System.out.print("Ingrese la nota del segundo semeste: ");
-        float nota2 = teclado.nextFloat();
-
-        float promedio = (nota1 + nota2) / 2;
-        System.out.println("El promedio de : " + nombreAlumno + " es de: " + promedio);
-
-
-
-
-
-        
 
         /**
          *Buena estartegia para solucionar un problema
