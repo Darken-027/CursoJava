80d67b8d8ccb5521624147d19543dbbaef51b7b6
Ahstell <torut1638@gmail.com>
2025-07-10 17:32:41 -0500

    Mensaje que indica al usuario que jugara piedra papel o tigera

--- src/main/java/org/example/areglos/PiedraPapelTigera.java
+++ src/main/java/org/example/areglos/PiedraPapelTigera.java
@@ -2,6 +2,6 @@ package org.example.areglos;
 
 public class PiedraPapelTigera {
     public static void main(String[] args) {
-        
+        System.out.println("Jugemos a piedra papel o tigera");
     }
 }
