02f9d2d3baa1c7be7d4bdf17c220152f4e6f9afa
Ahstell <torut1638@gmail.com>
2025-07-10 17:38:20 -0500

    Hacemos un bosquejo de como podria funcionar el programa

--- src/main/java/org/example/areglos/PiedraPapelTigera.java
+++ src/main/java/org/example/areglos/PiedraPapelTigera.java
@@ -3,5 +3,8 @@ package org.example.areglos;
 public class PiedraPapelTigera {
     public static void main(String[] args) {
         System.out.println("Jugemos a piedra papel o tigera");
+        System.out.println("Escribe alguno de estos: piedra, papel o tijeras");
+
+        System.out.println("La computadora escogio: papel");
     }
 }
