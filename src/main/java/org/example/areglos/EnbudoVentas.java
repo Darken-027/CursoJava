04af69bb45e0d46ee8d0b290448845c52d3074e4
Ahstell <torut1638@gmail.com>
2025-07-09 21:37:55 -0500

    Hacemos que el programa pierda clientes a medida que avanazan las etapas

--- src/main/java/org/example/areglos/EnbudoVentas.java
+++ src/main/java/org/example/areglos/EnbudoVentas.java
@@ -4,9 +4,13 @@ public class EnbudoVentas {
     public static void main(String[] args) {
 
         String etapa1 = "Se le envia un email";
+        int porcentaje1 = 25;
         String etapa2 = "El prospecto ingresa a la web";
+        int procentaje2 = 10;
         String etapa3 = "El prospecto ingresa a la seccion de compras";
+        int procentaje3 = 10;
         String etapa4 = "El prospecto compra el producto";
+        int procentaje4 = 10;
 
     }
 }
