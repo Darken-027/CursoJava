c2da956eb320fcc74bf96ca6308a386264bc780e
Ahstell <torut1638@gmail.com>
2025-07-09 22:20:41 -0500

    Creamos la formula para poder perder gente

--- src/main/java/org/example/areglos/EnbudoVentas.java
+++ src/main/java/org/example/areglos/EnbudoVentas.java
@@ -11,10 +11,14 @@ public class EnbudoVentas {
         String etapa2 = "El prospecto ingresa a la web";
         int procentaje2 = 10;
         prospectos = procentaje2 * prospectos / 100;
+
         String etapa3 = "El prospecto ingresa a la seccion de compras";
         int procentaje3 = 10;
+        prospectos = procentaje3 * prospectos / 100;
+
         String etapa4 = "El prospecto compra el producto";
-        int procentaje4 = 10;   
+        int procentaje4 = 10;
+        prospectos = procentaje4 * prospectos / 100;
 
     }
 }
