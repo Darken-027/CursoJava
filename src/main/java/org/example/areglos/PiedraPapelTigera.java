9f55e51e681280582fc561c99bbca543f2d389a6
Ahstell <torut1638@gmail.com>
2025-07-10 17:41:58 -0500

    llamamos a la clase Scanner

--- src/main/java/org/example/areglos/PiedraPapelTigera.java
+++ src/main/java/org/example/areglos/PiedraPapelTigera.java
@@ -1,10 +1,18 @@
 package org.example.areglos;
 
+import java.util.Scanner;
+
 public class PiedraPapelTigera {
     public static void main(String[] args) {
         System.out.println("Jugemos a piedra papel o tigera");
         System.out.println("Escribe alguno de estos: piedra, papel o tijeras");
 
+
+        //Pedirle al usuario que escoja
+        Scanner teclado = new Scanner(System.in);
+
+
+        //El usuario escribio papel
         System.out.println("La computadora escogio: papel");
         System.out.println("EMPATE!");
     }
