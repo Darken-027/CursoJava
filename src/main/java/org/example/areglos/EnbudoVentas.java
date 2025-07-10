536c6fa84e1c13f719d60ae91fc786f467cc9863
Ahstell <torut1638@gmail.com>
2025-07-09 21:34:55 -0500

    Se cambia el nombre de usuario por prospecto

--- src/main/java/org/example/areglos/EnbudoVentas.java
+++ src/main/java/org/example/areglos/EnbudoVentas.java
@@ -4,9 +4,9 @@ public class EnbudoVentas {
     public static void main(String[] args) {
 
         String etapa1 = "Se le envia un email";
-        String etapa2 = "El usuario ingresa a la web";
-        String etapa3 = "El usuario ingresa a la seccion de compras";
-        String etapa4 = "El usuario compra el producto";
+        String etapa2 = "El prospecto ingresa a la web";
+        String etapa3 = "El prospecto ingresa a la seccion de compras";
+        String etapa4 = "El prospecto compra el producto";
 
     }
 }
