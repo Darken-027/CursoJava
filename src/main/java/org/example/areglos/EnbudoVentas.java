4d43fc99dc8c8c449bfed07cc30720cb71fbf82b
Ahstell <torut1638@gmail.com>
2025-07-09 22:11:40 -0500

    El youtuber modifica todo el codigo de la nada :c que rabiaa

--- src/main/java/org/example/areglos/EnbudoVentas.java
+++ src/main/java/org/example/areglos/EnbudoVentas.java
@@ -2,13 +2,15 @@ package org.example.areglos;
 
 public class EnbudoVentas {
     public static void main(String[] args) {
-        int contactos = 10000;
+        int prospectos = 10000;
 
         String etapa1 = "Se le envia un email";
         int porcentaje1 = 25;
         int prospectos = (porcentaje1 * contactos) / 100;
+
         String etapa2 = "El prospecto ingresa a la web";
         int procentaje2 = 10;
+        int prospectos = (porcentaje2 * contactos) / 100;
         String etapa3 = "El prospecto ingresa a la seccion de compras";
         int procentaje3 = 10;
         String etapa4 = "El prospecto compra el producto";
