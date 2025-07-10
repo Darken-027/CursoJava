313952586e5217ebc101b97033856a62066260c9
Ahstell <torut1638@gmail.com>
2025-07-09 21:33:47 -0500

    Se escriben los mensajes que se mostraran en pantalla con el proceso paso a paso

--- src/main/java/org/example/areglos/EnbudoVentas.java
+++ src/main/java/org/example/areglos/EnbudoVentas.java
@@ -4,9 +4,9 @@ public class EnbudoVentas {
     public static void main(String[] args) {
 
         String etapa1 = "Se le envia un email";
-        String etapa2 = "Se le envia un email";
-        String etapa3 = "Se le envia un email";
-        String etapa4 = "Se le envia un email";
+        String etapa2 = "El usuario ingresa a la web";
+        String etapa3 = "El usuario ingresa a la seccion de compras";
+        String etapa4 = "El usuario compra el producto";
 
     }
 }
