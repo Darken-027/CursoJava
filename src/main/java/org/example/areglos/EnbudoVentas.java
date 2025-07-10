d2a66bf6e71acf19e5396181c72b99dd5b9811dc
Ahstell <torut1638@gmail.com>
2025-07-09 21:31:59 -0500

    Creamos los mensajes que queramos que vea el usuario para el embudo de ventas

--- src/main/java/org/example/areglos/EnbudoVentas.java
+++ src/main/java/org/example/areglos/EnbudoVentas.java
@@ -2,6 +2,11 @@ package org.example.areglos;
 
 public class EnbudoVentas {
     public static void main(String[] args) {
-        
+
+        String etapa1 = "Se le envia un email";
+        String etapa2 = "Se le envia un email";
+        String etapa3 = "Se le envia un email";
+        String etapa4 = "Se le envia un email";
+
     }
 }
