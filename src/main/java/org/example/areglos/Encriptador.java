7ee413747130bb8bd8df93c8199ed7deb9cdcad2
Ahstell <torut1638@gmail.com>
2025-07-11 16:10:09 -0500

    Configuramos el retorno de la funcion

--- src/main/java/org/example/areglos/Encriptador.java
+++ src/main/java/org/example/areglos/Encriptador.java
@@ -3,10 +3,12 @@ package org.example.areglos;
 public class Encriptador {
 
     public String encriptar(String texto) {
-
+        String resultado = "";
+        return resultado;
     }
 
     public String desencriptar(String texto) {
-        
+        String resultado = "";
+        return resultado;
     }
 }
