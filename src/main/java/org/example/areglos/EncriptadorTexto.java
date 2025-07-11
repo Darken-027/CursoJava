589d8aa22e2b3e88901731ec2a460075f3696e25
Ahstell <torut1638@gmail.com>
2025-07-11 15:55:21 -0500

    Creamos los mensajes que vera el usuario

--- src/main/java/org/example/areglos/EncriptadorTexto.java
+++ src/main/java/org/example/areglos/EncriptadorTexto.java
@@ -4,6 +4,8 @@ public class EncriptadorTexto {
     public static void main(String[] args) {
         String texto = "Hola Mundo";
         String textoEncriptado = encriptar(texto);
-        System.out.println("Texto encriptado: " + textoEncriptado);
+        System.out.println("Texto Encriptado: " + textoEncriptado);
+        String textoDesencriptado = desencriptar(textoEncriptado);
+        System.out.println("Texto Desencriptado: " + textoEncriptado);
     }
 }
