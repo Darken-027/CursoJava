f6cdef6e391b87c65777f11e2e1c7b80519f0b22
Ahstell <torut1638@gmail.com>
2025-07-11 14:48:20 -0500

    Declaramos las variables y la funcion para poder encriptar el texto

--- src/main/java/org/example/areglos/EncriptadorTexto.java
+++ src/main/java/org/example/areglos/EncriptadorTexto.java
@@ -2,6 +2,8 @@ package org.example.areglos;
 
 public class EncriptadorTexto {
     public static void main(String[] args) {
-        
+        String texto = "Hola Mundo";
+        String textoEncriptado = encriptar(texto);
+        System.out.println("Texto encriptado: " + textoEncriptado);
     }
 }
