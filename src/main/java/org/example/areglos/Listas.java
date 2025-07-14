044c07836b5c16861f5232c3f1f6b92653376f68
Darken-027 <torut1638@gmail.com>
2025-07-14 13:57:30 -0500

    Declarando una lista

--- src/main/java/org/example/areglos/Listas.java
+++ src/main/java/org/example/areglos/Listas.java
@@ -1,7 +1,10 @@
 package org.example.areglos;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Listas {
     public static void main(String[] args) {
-        
+        List<String> nombre = new ArrayList<>();
     }
 }
