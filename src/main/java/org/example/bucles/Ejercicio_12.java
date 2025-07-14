4eb9c85134030efd889f8a4f805e5c2393a3673a
Darken-027 <torut1638@gmail.com>
2025-07-14 18:11:46 -0500

    Exercise 12

---
+++ src/main/java/org/example/bucles/Ejercicio_12.java
@@ -0,0 +1,12 @@
+package org.example.bucles;
+
+public class Ejercicio_12 {
+    public static void main(String[] args) {
+        /*
+         *Write a program that asks the user for their age and displays on the screen all the
+         * years they have completed (from 1 to their age).
+         */
+
+        
+    }
+}
