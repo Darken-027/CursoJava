f4ede502e4206ad1d9878177bfab394fe339c340
Darken-027 <torut1638@gmail.com>
2025-07-14 14:36:10 -0500

    Creacion de los platillos

--- src/main/java/org/example/areglos/RecetasDeCocina.java
+++ src/main/java/org/example/areglos/RecetasDeCocina.java
@@ -2,6 +2,10 @@ package org.example.areglos;
 
 public class RecetasDeCocina {
     public static void main(String[] args) {
-        System.out.println("");
+        System.out.println("What do you want to cook");
+        System.out.println("Mofongo");
+        System.out.println("Fried ice cream");
+        System.out.println("Zurek");
+
     }
 }
