79e33e5cf8c9a79564d6d3bcebf5f756430660a4
Darken-027 <torut1638@gmail.com>
2025-07-14 15:56:35 -0500

    We geneted all constructors of the class Recipe

--- src/main/java/org/example/areglos/Recipe.java
+++ src/main/java/org/example/areglos/Recipe.java
@@ -4,5 +4,21 @@ import java.util.List;
 
 public class Recipe {
     private List<String> steps;
-    private List<String> ingredients;
+    private List<Ingredients> ingredients;
+
+    public List<Ingredients> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(List<Ingredients> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public List<String> getSteps() {
+        return steps;
+    }
+
+    public void setSteps(List<String> steps) {
+        this.steps = steps;
+    }
 }
