2f667dee581468ef434fa9a30a0f01be4047bd03
Darken-027 <torut1638@gmail.com>
2025-07-14 14:46:43 -0500

    I declare the steps that will make the program

--- src/main/java/org/example/areglos/RecetasDeCocina.java
+++ src/main/java/org/example/areglos/RecetasDeCocina.java
@@ -1,5 +1,7 @@
 package org.example.areglos;
 
+import java.util.Scanner;
+
 public class RecetasDeCocina {
     public static void main(String[] args) {
         System.out.println("What do you want to cook");
@@ -8,10 +10,16 @@ public class RecetasDeCocina {
         System.out.println("Zurek");
 
         //The user chooses an option
-
+        Scanner sc = new Scanner(System.in);
+        String chooseOption = sc.nextLine();
 
         //Showing the ingredients
+        System.out.println("Necessary Ingredients");
         System.out.println("- salt");
         System.out.println("- Water");
+
+        //Showing the steps
+        System.out.println("Steps");
+        System.out.println("- Heat a pan over low heat");
     }
 }
