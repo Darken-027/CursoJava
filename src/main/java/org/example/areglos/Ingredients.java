7a49235c9d092f2e9a2d81e32b5f9a510e615a3d
Darken-027 <torut1638@gmail.com>
2025-07-14 15:42:38 -0500

    We create all contructors

--- src/main/java/org/example/areglos/Ingredients.java
+++ src/main/java/org/example/areglos/Ingredients.java
@@ -4,4 +4,28 @@ public class Ingredients {
     private String name;
     private String amount;
     private UnitMeasurement nitMeasurement;
+
+    public String getAmount() {
+        return amount;
+    }
+
+    public void setAmount(String amount) {
+        this.amount = amount;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public UnitMeasurement getNitMeasurement() {
+        return nitMeasurement;
+    }
+
+    public void setNitMeasurement(UnitMeasurement nitMeasurement) {
+        this.nitMeasurement = nitMeasurement;
+    }
 }
