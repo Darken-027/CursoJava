215c5a99a3da13ff56a2e54f5b701eee6257904e
Darken-027 <torut1638@gmail.com>
2025-07-14 20:57:38 -0500

    Afected class

--- src/main/java/org/example/bucles/Ejercicio_12.java
+++ src/main/java/org/example/bucles/Ejercicio_12.java
@@ -1,12 +1,16 @@
 package org.example.bucles;
 
+import java.util.Scanner;
+
 public class Ejercicio_12 {
     public static void main(String[] args) {
         /*
          *Write a program that asks the user for their age and displays on the screen all the
          * years they have completed (from 1 to their age).
          */
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Cual es tu edad.");
 
-        
+
     }
 }
