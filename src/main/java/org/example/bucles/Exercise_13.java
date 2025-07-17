ae732c5647316eaa1eb16e1d99cf16f9c0c9dd0a
Darken-027 <torut1638@gmail.com>
2025-07-16 23:34:05 -0500

    Write the exercise

--- src/main/java/org/example/bucles/Exercise_13.java
+++ src/main/java/org/example/bucles/Exercise_13.java
@@ -2,6 +2,9 @@ package org.example.bucles;
 
 public class Exercise_13 {
     public static void main(String[] args) {
+        /*
+        *In this class I improve the exercise the age
+         */
         
     }
 }
