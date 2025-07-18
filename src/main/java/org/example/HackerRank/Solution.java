b7c2d1826eed95e6bd4001fd54a7c05b5936728d
Darken-027 <torut1638@gmail.com>
2025-07-18 16:10:41 -0500

    Add the solution class code

--- src/main/java/org/example/HackerRank/Solution.java
+++ src/main/java/org/example/HackerRank/Solution.java
@@ -2,6 +2,8 @@ package org.example.HackerRank;
 
 public class Solution {
     public static void main(String[] args) {
-        
+        /* Enter your code here. Print output to STDOUT. Your class should be named Solution. */
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
     }
 }
