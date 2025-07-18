1e6feb9a2d8c30426f8eba132e997c47547675b7
Darken-027 <torut1638@gmail.com>
2025-07-18 17:25:54 -0500

    Declaracion de la condicion if

--- src/main/java/org/example/estructurasDeControl/DeclaracionIf.java
+++ src/main/java/org/example/estructurasDeControl/DeclaracionIf.java
@@ -1,4 +1,13 @@
 package org.example.estructurasDeControl;
 
 public class DeclaracionIf {
+    public static void main(String[] args) {
+        int numero = 10;
+
+        //Comprueba si el numero es menor que 0
+        if(numero < 0){
+            System.out.println("El numero es negativo");
+        }
+        System.out.println("Sentencia fuera del bloque if");
+    }
 }
