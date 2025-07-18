b82d01fc477f1b656e3fb7d2cf488beb3102b862
Darken-027 <torut1638@gmail.com>
2025-07-18 17:48:01 -0500

    Se creo el codigo para resolver el problema

--- src/main/java/org/example/estructurasDeControl/Declaracion_if_else.java
+++ src/main/java/org/example/estructurasDeControl/Declaracion_if_else.java
@@ -2,6 +2,14 @@ package org.example.estructurasDeControl;
 
 public class Declaracion_if_else {
     public static void main(String[] args) {
-        
+        int numero = 10;
+        //Comprueva si el numero es mayor que 0
+        if(numero > 0){
+            System.out.println("El numero es positivo");
+        }
+        else{
+            System.out.println("El numero no es positivo");
+        }
+        System.out.println("Sentencia fuera del bloque if");
     }
 }
