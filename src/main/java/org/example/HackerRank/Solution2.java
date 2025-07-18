35491866166ca59ff6a8e78429b73924444e5430
Darken-027 <torut1638@gmail.com>
2025-07-18 16:24:18 -0500

    Printing on screen

---
+++ src/main/java/org/example/HackerRank/Solution2.java
@@ -0,0 +1,17 @@
+package org.example.HackerRank;
+
+import java.util.Scanner;
+
+public class Solution2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        System.out.println(a);
+
+        int b = scan.nextInt();
+        System.out.println(b);
+
+        int c = scan.nextInt();
+        System.out.println(c);
+    }
+}
