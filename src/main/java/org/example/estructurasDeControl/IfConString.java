206b6f6c8998457da3eb424332df1a76474d2175
Darken-027 <torut1638@gmail.com>
2025-07-18 17:40:51 -0500

    Creamos el codigo para resolver el problema

--- src/main/java/org/example/estructurasDeControl/IfConString.java
+++ src/main/java/org/example/estructurasDeControl/IfConString.java
@@ -2,6 +2,12 @@ package org.example.estructurasDeControl;
 
 public class IfConString {
     public static void main(String[] args) {
-        
+        //Crea la variable de tipo String
+        String lenguaje = "Java";
+
+        //Declaracion if
+        if(lenguaje == "Java"){
+            System.out.println("El mejor lenguaje de programacion");
+        }
     }
 }
