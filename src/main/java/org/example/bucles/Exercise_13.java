a18d36253bf66c146e80fea6ae3bbac6bf16b816
Darken-027 <torut1638@gmail.com>
2025-07-18 15:58:30 -0500

    Clase basia

--- src/main/java/org/example/bucles/Exercise_13.java
+++ src/main/java/org/example/bucles/Exercise_13.java
@@ -1,17 +1,7 @@
 package org.example.bucles;
 
-import java.util.Scanner;
-
 public class Exercise_13 {
     public static void main(String[] args) {
-        /*
-        *In this class I improve the exercise the age
-         */
-        int yearBirth = 0, currentYear = 0, age = 0;
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("How old are you. ");
-        yearBirth = sc.nextInt();       
-
+        
     }
 }
