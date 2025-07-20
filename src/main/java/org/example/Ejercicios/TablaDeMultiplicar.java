81d78e0d6df8a67c985d060a219a58cedeec9d5e
Darken-027 <torut1638@gmail.com>
2025-07-20 17:52:14 -0500

    Pedimos un numero al usuario

--- src/main/java/org/example/Ejercicios/TablaDeMultiplicar.java
+++ src/main/java/org/example/Ejercicios/TablaDeMultiplicar.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class TablaDeMultiplicar {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese un numero");
         int num = sc.nextInt();
 
         System.out.println("Tabla del " + num);
