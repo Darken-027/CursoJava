78676749740017dcdfe923c25cf8f134a56f3bc0
Darken-027 <torut1638@gmail.com>
2025-07-20 17:31:19 -0500

    Se creo el bucle for

--- src/main/java/org/example/Ejercicios/TablaDeMultiplicar.java
+++ src/main/java/org/example/Ejercicios/TablaDeMultiplicar.java
@@ -1,7 +1,14 @@
 package org.example.Ejercicios;
 
+import java.util.Scanner;
+
 public class TablaDeMultiplicar {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        System.out.println("Tabla del " + num);
+
+        for (int i = 1; i <= 10; i++) {}
     }
 }
