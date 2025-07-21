0d1fdd8b65fe849964885f32836eba9590fc87e4
Darken-027 <torut1638@gmail.com>
2025-07-20 23:55:04 -0500

    Creacion de los bucles for

--- src/main/java/org/example/Ejercicios/ImprimirTorreDeAsteriscos.java
+++ src/main/java/org/example/Ejercicios/ImprimirTorreDeAsteriscos.java
@@ -6,6 +6,12 @@ public class ImprimirTorreDeAsteriscos {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Ingresa el numero de filas de la torre.");
-        
+        int tam = sc.nextInt();
+
+        for(int altura = 1; altura < tam; altura++){
+            for(int i = 1; i <= (altura * 2) -1; i++){
+
+            }
+        }
     }
 }
