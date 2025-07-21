5317747e9bfbced19ed1d5d8941f33a848d543bf
Darken-027 <torut1638@gmail.com>
2025-07-21 01:14:44 -0500

    Modificamos la clase para que reconozcas letras mayuculas y minusculas

--- src/main/java/org/example/Ejercicios/DeterminarSiUnaCadenaEsPalindromo.java
+++ src/main/java/org/example/Ejercicios/DeterminarSiUnaCadenaEsPalindromo.java
@@ -6,13 +6,18 @@ public class DeterminarSiUnaCadenaEsPalindromo {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Ingrese la cadena. ");
-        String cadena = sc.next();
+        String cadena = sc.next().toLowerCase();
 
         String reversa = "";
 
         for (int i = cadena.length() -1; i >= 0; i--){
             reversa += cadena.charAt(i);
         }
-        System.out.println(reversa);
+
+        if(cadena.equals(reversa)){
+            System.out.println("Es palindromo");
+        }else {
+            System.out.println("No es un palindromo");
+        }
     }
 }
