3a9e91861f8859b2d1aba305135783a85e9f5327
Darken-027 <torut1638@gmail.com>
2025-07-21 02:27:28 -0500

    Coontinuamos el dia siguiente

--- src/main/java/org/example/Ejercicios/LlenadoArreglosNumeroAleatorios.java
+++ src/main/java/org/example/Ejercicios/LlenadoArreglosNumeroAleatorios.java
@@ -1,7 +1,15 @@
 package org.example.Ejercicios;
 
+import java.util.Random;
+
 public class LlenadoArreglosNumeroAleatorios {
     public static void main(String[] args) {
-        
+        int [] array = new int [10];
+        Random mRamdom = new Random();
+
+        for (int i = 0; i < array.length; i++){
+            array[i] = mRamdom.nextInt(100);
+            System.out.println("Array ");
+        }
     }
 }
