559b715b23e8c3425414fe920ace889e03e48097
Darken-027 <torut1638@gmail.com>
2025-07-21 00:58:14 -0500

    Pedimos la cadena al usuari

--- src/main/java/org/example/Ejercicios/DeterminarSiUnaCadenaEsPalindromo.java
+++ src/main/java/org/example/Ejercicios/DeterminarSiUnaCadenaEsPalindromo.java
@@ -1,7 +1,13 @@
 package org.example.Ejercicios;
 
+import java.util.Scanner;
+
 public class DeterminarSiUnaCadenaEsPalindromo {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese la cadena. ");
+        String cadena = sc.next();
+
         
     }
 }
