86ff490d8a9a2e6ffb27e7138a59ea1a612d293a
Darken-027 <torut1638@gmail.com>
2025-07-20 23:31:42 -0500

    Pedimos al usuario la cantidad de asteriscos

--- src/main/java/org/example/Ejercicios/ImprimirTorreDeAsteriscos.java
+++ src/main/java/org/example/Ejercicios/ImprimirTorreDeAsteriscos.java
@@ -1,7 +1,11 @@
 package org.example.Ejercicios;
 
+import java.util.Scanner;
+
 public class ImprimirTorreDeAsteriscos {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingresa el numero de filas de la torre.");
         
     }
 }
