309a5ada660c598dd658a34e3912e3879cc55543
Darken-027 <torut1638@gmail.com>
2025-07-21 13:46:34 -0500

    Finalizacion de la codificacion

--- src/main/java/org/example/Ejercicios/LlenarUnArregloConNumerosAleatorios2.java
+++ src/main/java/org/example/Ejercicios/LlenarUnArregloConNumerosAleatorios2.java
@@ -1,7 +1,19 @@
 package org.example.Ejercicios;
 
+import java.util.Scanner;
+
 public class LlenarUnArregloConNumerosAleatorios2 {
     public static void main(String[] args) {
-        
+        int [] array = new int[10];
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 0; i < array.length; i++){
+            System.out.println("Ingresa un numero ");
+            array[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < array.length; i++){
+            System.out.println("Array ( " + (i + 1) + " )" + " = " + array[i]);
+        }
     }
 }
