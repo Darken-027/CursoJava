9df65dc9a79a4811de95498c90c7afbc78e1647d
Darken-027 <torut1638@gmail.com>
2025-07-21 15:50:10 -0500

    Se declararon todos los metodos que se usaran en esta clase

---
+++ src/main/java/org/example/metodos/OperacionPrincipal.java
@@ -0,0 +1,38 @@
+package org.example.metodos;
+
+public class OperacionPrincipal {
+
+    //ATRIBUTOS
+    int suma;
+    int resta;
+    int multiplicacion;
+    int divisioon;
+
+
+    //METODOS
+
+    //METODO PARA SUMAR AMBOS NUMEROS
+    public void sumar(){
+
+    }
+
+    //METODO PARA RESTAR AMBOS NUMEROS
+    public void restar(){
+
+    }
+
+    //METODO PARA MULTIPLICAR AMBOS NUMEROS
+    public void multiplicar(){
+
+    }
+
+    //METODO PARA DIVIDIR AMBOS NUMEROS
+    public void devidir(){
+
+    }
+
+    //METODO PARA MOSTRAR EL RESULTADO
+    public void mostrarResultado(){
+
+    }
+}
--- src/main/java/org/example/metodos/Principal.java
+++ src/main/java/org/example/metodos/Principal.java
@@ -2,6 +2,6 @@ package org.example.metodos;
 
 public class Principal {
     public static void main(String[] args) {
-        
+
     }
 }
