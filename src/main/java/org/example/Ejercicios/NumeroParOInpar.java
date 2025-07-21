c48ceaf75d39baad5787fd5c6893cc8a5345d8f8
Darken-027 <torut1638@gmail.com>
2025-07-21 00:25:05 -0500

    Se finalizo con la codificacion

--- src/main/java/org/example/Ejercicios/NumeroParOInpar.java
+++ src/main/java/org/example/Ejercicios/NumeroParOInpar.java
@@ -1,7 +1,18 @@
 package org.example.Ejercicios;
 
+import java.util.Scanner;
+
 public class NumeroParOInpar {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese un numero: ");
+        int numero = sc.nextInt();
+
+        if(numero % 2 == 0 ){
+            System.out.println("ES PAR");
+        }
+        else{
+            System.out.println("ES IMPAR");
+        }
     }
 }
