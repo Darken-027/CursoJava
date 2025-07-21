fb39c07ae7f710e35dadc8080dcc3ae79fe41a0d
Darken-027 <torut1638@gmail.com>
2025-07-20 23:18:14 -0500

    Creando el codigo para generar todas las tablas de multiplicar

--- src/main/java/org/example/Ejercicios/TablaDeMultiplicar.java
+++ src/main/java/org/example/Ejercicios/TablaDeMultiplicar.java
@@ -5,13 +5,20 @@ import java.util.Scanner;
 public class TablaDeMultiplicar {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Ingrese un numero entre 1 y 10");
+        /*System.out.println("Ingrese un numero entre 1 y 10");
         int num = sc.nextInt();
 
         System.out.println("Tabla del " + num);
 
         for (int i = 1; i <= 10; i++) {
             System.out.println( num + " X " + i + " = " + num * i);
+        }*/
+
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= 10; j++) {
+                System.out.println(i + " X " + j + " = " + i * j);
+            }
+            System.out.println("");
         }
     }
 }
