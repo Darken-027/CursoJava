0c2b663e42035d88cb2d9b59287636d56acca4d9
Darken-027 <torut1638@gmail.com>
2025-07-21 11:25:08 -0500

    Se termino con la codificacion del problema

--- src/main/java/org/example/Ejercicios/LlenarUnArregloConNumerosAleatorios.java
+++ src/main/java/org/example/Ejercicios/LlenarUnArregloConNumerosAleatorios.java
@@ -1,7 +1,15 @@
 package org.example.Ejercicios;
 
+import java.util.Random;
+
 public class LlenarUnArregloConNumerosAleatorios {
     public static void main(String[] args) {
-        
+        int [] array = new int[10];
+        Random mRandom = new Random();
+
+        for (int i = 0; i < array.length; i++){
+            array[i] = mRandom.nextInt(100);
+            System.out.println("Array (" + (i + 1) + " = " + array[i]);
+        }
     }
 }
