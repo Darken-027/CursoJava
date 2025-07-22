785238f67de86acfb40a68ad0cd11cc7209b81b0
Darken-027 <torut1638@gmail.com>
2025-07-21 22:32:57 -0500

    Se finalizo con la codificacion

--- src/main/java/org/example/arreglos/Arreglo.java
+++ src/main/java/org/example/arreglos/Arreglo.java
@@ -2,6 +2,20 @@ package org.example.arreglos;
 
 public class Arreglo {
     public static void main(String[] args) {
-        
+        String [] dias = new String[7];
+        dias[0] = "Domingo";
+        dias[1] = "Lunes";
+        dias[2] = "Martes";
+        dias[3] = "Miercoles";
+        dias[4] = "Jueves";
+        dias[5] = "Viernes";
+        dias[6] = "Sabado";
+
+        for(int i = 0; i < dias.length; i++){
+            System.out.println("" + dias[i]);
+        }
+
+        System.out.println("Dia: " + dias[3]);
+        System.out.println("Items: " + dias[3].length());
     }
 }
