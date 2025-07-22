5fbfb5536d5084e3d694f4397a34d16ebd136a99
Darken-027 <torut1638@gmail.com>
2025-07-22 14:57:02 -0500

    Creando diferentes formas para llenar un arreglo en java

--- src/main/java/org/example/arreglos/app.java
+++ src/main/java/org/example/arreglos/app.java
@@ -1,7 +1,28 @@
 package org.example.arreglos;
 
+import java.util.Scanner;
+
 public class app {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        String [] apellidos = new String[5];
+
+        apellidos[0] = "Hamby";
+        apellidos[1] = "Maria";
+        apellidos[2] = "Luiz";
+        apellidos[3] = "Rio";
+        apellidos[4] = "Sao Paulo";
+
+        for (int i = 0; i < apellidos.length; i++){
+            System.out.println(apellidos[i]);
+        }
+
+        int precios [] = new int[2];
+
+        for (int i = 0; i < precios.length; i++){
+            System.out.println("Dame un precio: ");
+            precios[0] = sc.nextInt();
+        }
     }
 }
