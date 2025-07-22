2292d3a571676e258a7e41bf2ca1cbf113dc62af
Darken-027 <torut1638@gmail.com>
2025-07-21 22:07:55 -0500

    Finalizacion de la codificacion

--- src/main/java/org/example/metodos/Principal.java
+++ src/main/java/org/example/metodos/Principal.java
@@ -1,7 +1,18 @@
 package org.example.metodos;
 
+import javax.swing.*;
+
 public class Principal {
     public static void main(String[] args) {
+        int n1 = Integer.parseInt(JOptionPane.showInputDialog("Digite un numero"));
+        int n2 = Integer.parseInt(JOptionPane.showInputDialog("Digite un numero"));
 
+        OperacionPrincipal op = new OperacionPrincipal ();
+
+        op.sumar(n1,n2);
+        op.restar(n1,n2);
+        op.multiplicar(n1,n2);
+        op.devidir(n1,n2);
+        op.mostrarResultado();
     }
 }
