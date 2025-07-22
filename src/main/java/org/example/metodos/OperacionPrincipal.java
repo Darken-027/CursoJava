d250aa7207237a09fe1f14d23b1ffac688381063
Darken-027 <torut1638@gmail.com>
2025-07-21 22:01:18 -0500

    Se modifico la clase

--- src/main/java/org/example/metodos/OperacionPrincipal.java
+++ src/main/java/org/example/metodos/OperacionPrincipal.java
@@ -3,32 +3,33 @@ package org.example.metodos;
 public class OperacionPrincipal {
 
     //ATRIBUTOS
+    //VARIABLES GLOBALES ES CUANDO ESTAN FUERA DE UN METODO
     int suma;
     int resta;
     int multiplicacion;
-    int divisioon;
+    int division;
 
 
     //METODOS
 
     //METODO PARA SUMAR AMBOS NUMEROS
-    public void sumar(){
-
+    public void sumar(int numero1, int numero2){
+        suma = numero1 + numero2;
     }
 
     //METODO PARA RESTAR AMBOS NUMEROS
-    public void restar(){
-
+    public void restar(int numero1, int numero2){
+        resta = numero1 - numero2;
     }
 
     //METODO PARA MULTIPLICAR AMBOS NUMEROS
-    public void multiplicar(){
-
+    public void multiplicar(int numero1, int numero2){
+        multiplicacion = numero1 * numero2;
     }
 
     //METODO PARA DIVIDIR AMBOS NUMEROS
-    public void devidir(){
-
+    public void devidir(int numero1, int numero2){
+        division = numero1 / numero2;
     }
 
     //METODO PARA MOSTRAR EL RESULTADO
@@ -36,6 +37,6 @@ public class OperacionPrincipal {
         System.out.println("La suma es: " + suma);
         System.out.println("La resta es: " + resta);
         System.out.println("La multiplicacion es: " + multiplicacion);
-        System.out.println("La division es: " + divisioon);
+        System.out.println("La division es: " + division);
     }
 }
