c9ba7d1cdeae33bd2dc0082d3f0378869c73ccf7
Darken-027 <torut1638@gmail.com>
2025-07-23 22:50:28 -0500

    Se creo la variable numero

--- src/main/java/org/example/arreglosLABDA/NumeroParImpar.java
+++ src/main/java/org/example/arreglosLABDA/NumeroParImpar.java
@@ -2,6 +2,10 @@ package org.example.arreglosLABDA;
 
 public class NumeroParImpar {
     public static void main(String[] args) {
+        
+        int numero = 0;
         System.out.println("Dame un numero");
+
+
     }
 }
