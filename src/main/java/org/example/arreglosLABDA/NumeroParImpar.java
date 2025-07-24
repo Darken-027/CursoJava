f5469747a8826d4b5400dd9f34568e4607e7a3cb
Darken-027 <torut1638@gmail.com>
2025-07-23 23:04:51 -0500

    Mostramos el caso contrario de la condicional al usuario

--- src/main/java/org/example/arreglosLABDA/NumeroParImpar.java
+++ src/main/java/org/example/arreglosLABDA/NumeroParImpar.java
@@ -15,6 +15,8 @@ public class NumeroParImpar {
 
         if(resultado == 0){
             System.out.println("El numero es par ");
+        } else {
+            System.out.println("El numero es impar ");
         }
     }
 }
