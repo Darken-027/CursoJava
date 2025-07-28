68152f48045e6c9bf3cbf8838186f6be4132f67d
Darken-027 <torut1638@gmail.com>
2025-07-28 12:15:29 -0500

    Creamos los metodos de la clase auto

---
+++ src/main/java/org/example/POO/auto.java
@@ -0,0 +1,11 @@
+package org.example.POO;
+
+public class auto {
+    public void acelerar(){
+        System.out.println("Acelerando...");
+    }
+
+    public void velocidad(int vel_max){
+        System.out.println("La velocidad maxima es: " + vel_max);
+    }
+}
