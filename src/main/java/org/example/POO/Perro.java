7997891169a7de69423c0f3e64992cb8e418cd04
Darken-027 <torut1638@gmail.com>
2025-07-28 10:23:24 -0500

    Se crearon los atributos de la clase perro

---
+++ src/main/java/org/example/POO/Perro.java
@@ -0,0 +1,14 @@
+package org.example.POO;
+
+public class Perro {
+    int ID;
+    String raza;
+    int precio;
+
+    //Contructor
+    public Perro (int ID, String raza, int precio){
+        this.ID = ID;
+        this.raza = raza;
+        this.precio = precio;
+    }
+}
