9097a4a585bbfe1e73945c1465953e712004a998
Darken-027 <torut1638@gmail.com>
2025-08-01 17:28:22 -0500

    Se creo el contructor y los atrubuctos de la clase Raices

--- src/main/java/org/example/POO/raices/Principal.java
+++ src/main/java/org/example/POO/raices/Principal.java
@@ -2,6 +2,6 @@ package org.example.POO.raices;
 
 public class Principal {
     public static void main(String[] args) {
-         
+
     }
 }
--- src/main/java/org/example/POO/raices/Raices.java
+++ src/main/java/org/example/POO/raices/Raices.java
@@ -1,5 +1,13 @@
 package org.example.POO.raices;
 
 public class Raices {
+    private double a;
+    private double b;
+    private double c;
 
+    public Raices(double a, double b, double c){
+        this.a = a;
+        this.b = b;
+        this.c =c;
+    }
 }
