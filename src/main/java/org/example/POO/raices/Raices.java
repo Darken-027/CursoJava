3a4a654d3448d8f81d42f6f850496894c8587be0
Darken-027 <torut1638@gmail.com>
2025-08-01 17:38:06 -0500

    Se crearon los metodos de la clase

--- src/main/java/org/example/POO/raices/Raices.java
+++ src/main/java/org/example/POO/raices/Raices.java
@@ -1,4 +1,20 @@
 package org.example.POO.raices;
+/*
+    Enunciado:
+
+    Vamos a realizar una clase llamada Raices, donde representaremos los valores de una ecuación de 2º grado.
+    Tendremos los 3 coeficientes como atributos, llamémosles a, b y c.
+    Hay que insertar estos 3 valores para construir el objeto.
+    Las operaciones que se podrán hacer son las siguientes:
+    obtenerRaices(): imprime las 2 posibles soluciones
+    obtenerRaiz(): imprime única raíz, que será cuando solo tenga una solución posible.
+    getDiscriminante(): devuelve el valor del discriminante (double), el discriminante tiene la siguiente formula, b2-4ac
+    tieneRaices(): devuelve un booleano indicando si tiene dos soluciones, para que esto ocurra, el discriminante debe ser mayor o igual que 0.
+    tieneRaiz(): devuelve un booleano indicando si tiene una única solución, para que esto ocurra, el discriminante debe ser igual que 0.
+    calcular(): mostrara por consola las posibles soluciones que tiene nuestra ecuación, en caso de no existir solución, mostrarlo también.
+    Formula ecuación 2º grado:  (-b±√(b^2-4ac))/2a
+    Solo varia el signo delante de -b
+*/
 
 public class Raices {
     private double a;
@@ -10,4 +26,24 @@ public class Raices {
         this.b = b;
         this.c =c;
     }
+
+    private void obtenerRaices(){
+
+    }
+    private void obtenerRaiz(){
+
+    }
+
+    private double getDiscriminante(){
+        return Math.pow(b, 2) - (4 * a * c);
+    }
+
+    private boolean tieneRaices(){
+        return getDiscriminante() >= 0;
+    }
+
+    private boolean tieneRaiz(){
+        return getDiscriminante() == 0;
+    }
+
 }
