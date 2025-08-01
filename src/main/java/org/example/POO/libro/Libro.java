10be49aa18d05660e24d43b188bbf4bb4e088e02
Darken-027 <torut1638@gmail.com>
2025-08-01 15:52:10 -0500

    Se mofico la clase libro para probar errores y se finalizo con la codificacion de este ejercicio

--- src/main/java/org/example/POO/libro/Libro.java
+++ src/main/java/org/example/POO/libro/Libro.java
@@ -1,5 +1,21 @@
 package org.example.POO.libro;
 
+/*
+    Enunciado:
+
+            1. Crear una clase Libro que contenga los siguientes atributos:
+    ISBN
+            Titulo
+    Autor
+    Número de páginas
+    Crear sus respectivos métodos get y set correspondientes para cada atributo.
+    Crear el método toString() para mostrar la información relativa al libro con el siguiente formato:
+            "El libro su_titulo con ISBN su_ISBN creado por el autor su_autor tiene num_paginas páginas"
+    En el fichero main, crear 2 objetos Libro (los valores que se quieran) y mostrarlos por pantalla.
+    Por último, indicar cuál de los 2 tiene más páginas.
+*/
+
+
 public class Libro {
 
     //Atributos
@@ -50,5 +66,14 @@ public class Libro {
         this.numpaginas = numpaginas;
     }
 
+    @Override
+    public String toString(){
+        return "El libro " + titulo + " con ISBN " + ISBN + " creado por el autor " + autor + ""
+                + " creado por el autor " + autor
+                + " tiene " + numpaginas + " paginas";
+    }
 
 }
+
+
+
--- src/main/java/org/example/POO/libro/Principal.java
+++ src/main/java/org/example/POO/libro/Principal.java
@@ -4,9 +4,18 @@ public class Principal {
     public static void main(String[] args) {
 
         Libro libro1 = new Libro(3164, "titulo1", "autor1", 30);
-        Libro libro2 = new Libro(2541, "titulo2", "autor2", 100);
+        Libro libro2 = new Libro(2541, "titulo2", "autor2", 20);
 
+        
         System.out.println(libro1.toString());
-        System.out.println(libro1.toString());
+        System.out.println(libro2.toString());
+
+        libro1.setNumpaginas(80);
+
+        if(libro1.getNumpaginas() > libro2.getNumpaginas()){
+            System.out.println(libro1.getTitulo() + " tiene mas paginas");
+        } else {
+            System.out.println(libro2.getTitulo() + " tiene mas paginas");
+        }
     }
 }
