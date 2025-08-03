1361cda10d6e3db983b7936c3adf1c4f484bcb55
Darken-027 <torut1638@gmail.com>
2025-08-03 16:02:48 -0500

    Se creo la clase profesor

--- src/main/java/org/example/POO/alumnos_y_profesores/Aula.java
+++ src/main/java/org/example/POO/alumnos_y_profesores/Aula.java
@@ -1,5 +1,8 @@
 package org.example.POO.alumnos_y_profesores;
 
 public class Aula {
-    
+
+    private Profesor profesor;
+    private Alumno[] alumnos;
+    private String materias;
 }
---
+++ src/main/java/org/example/POO/alumnos_y_profesores/Profesor.java
@@ -0,0 +1,4 @@
+package org.example.POO.alumnos_y_profesores;
+
+public class Profesor {
+}
