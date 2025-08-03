ee44e9afe77768c4700928fc627e6e55384be89e
Darken-027 <torut1638@gmail.com>
2025-08-03 16:28:13 -0500

    Se creo el metodo para generar un numero aleatorio dentro de la clase MetodosSueltos

--- src/main/java/org/example/POO/alumnos_y_profesores/MetodosSueltos.java
+++ src/main/java/org/example/POO/alumnos_y_profesores/MetodosSueltos.java
@@ -1,5 +1,8 @@
 package org.example.POO.alumnos_y_profesores;
 
 public class MetodosSueltos {
-    
+    public static int generaNumeroAleatorio(int minimo, int maximo){
+        int num = (int)Math.floor(Math.random() * (minimo - (maximo + 1)) + (maximo + 1));
+        return num;
+    }
 }
