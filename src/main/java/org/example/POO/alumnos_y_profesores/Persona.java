2fb4b40a77d0c0c37970a11c97f70894df6df779
Darken-027 <torut1638@gmail.com>
2025-08-04 08:02:47 -0500

    Se crearon las constantes final de nombres de chicos y de nombres de chicas dentro de la clase persona

--- src/main/java/org/example/POO/alumnos_y_profesores/Persona.java
+++ src/main/java/org/example/POO/alumnos_y_profesores/Persona.java
@@ -5,6 +5,10 @@ public class Persona {
     private char sexo;
     private int edad;
 
+    private final String[] nombres_chicos = {"pepe", "fernando", "alberto", "nacho"};
+    private final String[] nombres_chicas = {"alicia", "juana", "alberta", "urelia"};
+
+    
     public Persona(){
 
     }
