e0e00b6a47c4d92627dec3f0922e9706a0ec4fda
Darken-027 <torut1638@gmail.com>
2025-08-04 13:53:17 -0500

    Clase afectada

--- src/main/java/org/example/POO/alumnos_y_profesores/Persona.java
+++ src/main/java/org/example/POO/alumnos_y_profesores/Persona.java
@@ -9,15 +9,18 @@ public class Persona {
     private final String[] NOMBRES_CHICOS = {"pepe", "fernando", "alberto", "nacho"};
     private final String[] NOMBRES_CHICAS = {"alicia", "juana", "alberta", "urelia"};
     private final int CHICO = 0;
-    private final int CHICAS = 0;
+    private final int CHICAS = 1;
 
 
     public Persona(){
         int determinarSexo = MetodosSueltos.generaNumeroAleatorio(0,1);
+
         if (determinarSexo == 0) {
             nombre = NOMBRES_CHICOS[MetodosSueltos.generaNumeroAleatorio(0,4)];
+            sexo = 'H'
         }else {
             nombre = NOMBRES_CHICAS[MetodosSueltos.generaNumeroAleatorio(0,4)];
+            sexo = 'M';
         }
 
 
