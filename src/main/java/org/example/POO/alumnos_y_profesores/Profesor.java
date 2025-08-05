e8b4408fb6c176e6925e7753b7be7a2f3d5d904b
Darken-027 <torut1638@gmail.com>
2025-08-04 23:02:18 -0500

    Clase afectada

--- src/main/java/org/example/POO/alumnos_y_profesores/Profesor.java
+++ src/main/java/org/example/POO/alumnos_y_profesores/Profesor.java
@@ -2,4 +2,12 @@ package org.example.POO.alumnos_y_profesores;
 
 public class Profesor extends Persona{
     private String materias;
+
+    public Profesor(){
+        super();
+
+        super.setEdad(MetodosSueltos.generaNumeroAleatorio(25,40));
+
+
+    }
 }
