b862d364f1ea655857f659d4615eac12531aaf10
Darken-027 <torut1638@gmail.com>
2025-08-05 00:19:51 -0500

    codificamos en el main

--- src/main/java/org/example/POO/alumnos_y_profesores/Principal.java
+++ src/main/java/org/example/POO/alumnos_y_profesores/Principal.java
@@ -2,6 +2,9 @@ package org.example.POO.alumnos_y_profesores;
 
 public class Principal {
     public static void main(String[] args) {
-
+        Aula aula = new Aula();
+        if(aula.darClase()){
+            aula.notas();
+        }
     }
 }
