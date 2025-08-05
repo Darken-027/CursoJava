24ed68059d612078cf30c474c30d5ea0dc0c7484
Darken-027 <torut1638@gmail.com>
2025-08-04 23:47:02 -0500

    Se afecto la clase Aula

--- src/main/java/org/example/POO/alumnos_y_profesores/Aula.java
+++ src/main/java/org/example/POO/alumnos_y_profesores/Aula.java
@@ -2,7 +2,30 @@ package org.example.POO.alumnos_y_profesores;
 
 public class Aula {
 
+    private int id;
     private Profesor profesor;
     private Alumno[] alumnos;
-    private String materias;
+    private String materia;
+
+    private final int MAX_ALUMNOS = 20;
+
+    public Aula(){
+
+        id = 1;
+
+        profesor = new Profesor();
+        alumnos = new Alumno[MAX_ALUMNOS];
+        creaAlumno();
+        materia = Constantes.MATERIAS[MetodosSueltos.generaNumeroAleatorio(0,2)];
+    }
+
+    private void creaAlumno(){
+        for(int i = 0; i < alumnos.length; i++){
+            alumnos[i] = new Alumno();
+        }
+    }
+
+    public boolean darClase(){
+        
+    }
 }
